7e909f4e4548ce50edc75a30ce5a686780208c2a
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-03 16:42:11 +1100

    feat: add button to set new grid size

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const container = document.querySelector(".container");
 
-let gridSize = 16*16;
+let defaultGridSize = 16;
 
 function generateGrid(gridSize) {
-for (let i = 0; i < (gridSize); i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class","square");
-    div.addEventListener("mouseover",() => div.style.backgroundColor = "black");
-    container.appendChild(div);
+    for (let i = 0; i < (gridSize*gridSize); i++) {
+        const div = document.createElement("div");
+        div.setAttribute("class","square");
+        div.style.flexBasis = `${100/gridSize}%`;
+        div.addEventListener("mouseover",() => div.style.backgroundColor = "black");
+        container.appendChild(div);
     }
 }
 
@@ -16,3 +17,14 @@ function removeGrid() {
         container.removeChild(container.firstChild);
     }
 }
+
+generateGrid(defaultGridSize);
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let newGridSize = +prompt('Enter new grid size',defaultGridSize);
+    if (newGridSize && newGridSize <= 100) {
+        removeGrid();
+        generateGrid(newGridSize);
+    }
+})
