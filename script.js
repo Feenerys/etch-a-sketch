e29e2303d3255efb7c432a61538a87b6b2ef64b8
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-02-03 16:09:47 +1100

    refractor: make grid creation a function

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const container = document.querySelector(".container");
 
 let gridSize = 16*16;
 
+function generateGrid(gridSize) {
 for (let i = 0; i < (gridSize); i++) {
     const div = document.createElement("div");
     div.setAttribute("class","square");
     div.addEventListener("mouseover",() => div.style.backgroundColor = "black");
     container.appendChild(div);
+    }
 }
 
